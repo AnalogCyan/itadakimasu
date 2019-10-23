b3cd7e1a226bb7bc85ed3dcd3411eb588870bdda
Cyan Thayn <git@thayn.me>
2019-10-23 02:41:10 -0500

    Initial commit

---
+++ generate.js
@@ -0,0 +1,25 @@
+var foods = [
+  'alfalfa', 'clover', 'pea', 'bean', 'lentil', 'lupin', 'mesquite', 'carob', 'soybean', 'peanut', 'tamarind', 'acorn', 'almond', 'beech', 'candlenut', 'cashew', 'chestnut', 'hazelnut', 'pecan', 'macadamia', 'pistachio', 'walnut', 'asparagus', 'apple', 'avacado', 'squash', 'arugala', 'artichoke', 'applesauce', 'noodle', 'cantelope', 'tuna', 'juice', 'sushi', 'bruscetta', 'bacon', 'bagel', 'bbq', 'bison', 'barley', 'beer', 'bisque', 'bluefish', 'bread', 'broccoli', 'buritto', 'babaganoosh', 'cabbage', 'cake', 'carrots', 'celery', 'cheese', 'chicken', 'catfish', 'chips', 'chocolate', 'chowder', 'clam', 'coffee', 'cookie', 'corn', 'cupcake', 'crab', 'curry', 'cereal', 'chimichanga', 'date', 'dip', 'duck', 'dumpling', 'donut', 'egg', 'enchilada', 'eggroll', 'muffin', 'edamame', 'eel', 'fajita', 'falafel', 'fish', 'fondu', 'toast', 'garlic', 'ginger', 'gnocchi', 'goose', 'granola', 'grape', 'guancamole', 'gumbo', 'grits', 'cracker', 'ham', 'halibut', 'hamburger', 'cheeseburger', 'honey', 'sugar', 'roll', 'hummus', 'ice', 'cream', 'stew', 'jambalaya', 'jelly', 'jam', 'jerky', 'jalapeño', 'kale', 'ketchup', 'kiwi', 'kingfish', 'lobster', 'lamb', 'linguine', 'lasagna', 'meatball', 'moose', 'milk', 'milkshake', 'ostrich', 'pizza', 'pepperoni', 'pancakes', 'quesadilla', 'quiche', 'reuben', 'spinach', 'spaghetti', 'venison', 'waffle', 'wine', 'yogurt', 'ziti', 'zucchini', 'water', 'vodka', 'cocktail', 'rhubarb', 'martini', 'watermelon', 'melon', 'strawberry', 'grape', 'raspberry'
+]
+
+var button = [
+  'Generate', 'Generate Recipe', 'Generate Password', 'Generate Recipe Password', 'Another!', 'Not your cup of tea?'
+]
+
+var placeholders = [
+  'correct-horse-battery-staple', 'water-rhubarb-martini'
+]
+
+function starter() {
+  document.getElementById('pass').innerHTML = placeholders[Math.floor(Math.random() * (placeholders.length))];
+}
+
+function pass() {
+  var randFood1 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood2 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood3 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood4 = foods[Math.floor(Math.random() * (foods.length))];
+  var seperator = "-"
+  document.getElementById('pass').innerHTML = randFood1.concat(seperator.concat(randFood2.concat(seperator.concat(randFood3.concat(seperator.concat(randFood4))))));
+  document.getElementById('gen').innerHTML = button[Math.floor(Math.random() * (button.length))];
+}
