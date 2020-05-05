142b2f9298f4a771fc670fa4a5a69b39f8c292ac
Cyan Thayn <git@thayn.me>
2020-05-05 13:19:50 -0500

    Revert "Moved food list, fixes #6"
    This reverts commit a58574de1e1a8693e7e865d8e3629be20eb446ff, broke PWA.

--- generate.js
+++ generate.js
@@ -1,13 +1,6 @@
-function loadFile(filePath) {
-  var result = null;
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET", filePath, false);
-  xmlhttp.send();
-  if (xmlhttp.status == 200) {
-    result = xmlhttp.responseText;
-  }
-  return result;
-}
+var foods = [
+  "alfalfa", "shawarma", "gin", "rum", "tea", "tomago", "beetroot", "tunip", "cardamom", "fried", "boiled", "baked", "steamed", "broiled", "sake", "tequila", "blueberry", "biryani", "clover", "apricot", "pea", "bean", "lentil", "lupin", "mesquite", "carob", "soybean", "peanut", "tamarind", "acorn", "almond", "beech", "candlenut", "cashew", "chestnut", "hazelnut", "pecan", "macadamia", "pistachio", "walnut", "asparagus", "apple", "avacado", "squash", "arugala", "artichoke", "applesauce", "noodle", "cantelope", "tuna", "juice", "sushi", "bruscetta", "bacon", "bagel", "bbq", "bison", "barley", "beer", "bisque", "bluefish", "bread", "broccoli", "buritto", "babaganoosh", "cabbage", "cake", "carrots", "celery", "cheese", "chicken", "catfish", "chips", "chocolate", "chowder", "clam", "coffee", "cookie", "corn", "cupcake", "crab", "curry", "cereal", "chimichanga", "date", "dip", "duck", "dumpling", "donut", "egg", "enchilada", "eggroll", "muffin", "edamame", "eel", "fajita", "falafel", "fish", "fondu", "toast", "garlic", "ginger", "gnocchi", "goose", "granola", "grape", "guacamole", "gumbo", "grits", "cracker", "ham", "halibut", "hamburger", "cheeseburger", "honey", "sugar", "roll", "hummus", "ice", "cream", "stew", "jambalaya", "jelly", "jam", "jerky", "jalapeño", "kale", "ketchup", "kiwi", "kingfish", "lobster", "lamb", "linguine", "lasagna", "meatball", "moose", "milk", "milkshake", "ostrich", "pizza", "pepperoni", "pancakes", "quesadilla", "quiche", "reuben", "spinach", "spaghetti", "venison", "waffle", "wine", "yogurt", "ziti", "zucchini", "water", "vodka", "cocktail", "rhubarb", "martini", "watermelon", "melon", "strawberry", "raspberry", "banana", "cherry", "lemon", "plum", "pumpkin", "potato", "eggplant", "onion", "mushroom", "sausage", "steak", "porridge", "rice", "seafood", "cod", "herring", "salmon", "maize", "wheat", "flour", "musturd", "crisp", "syrup", "candy", "dessert", "kebab", "kombucha", "pie", "salad", "letuce", "sandwich", "sauce", "soy", "snack", "raisin", "soup", "cider", "soda", "pop", "alcohol", "lemonade", "marshmallow"
+];
 
 var button = [
   "Generate", "Generate Recipe", "Generate Password", "Generate Recipe Password", "Generate Random Password", "Generate Random Recipe", "Another!", "Not your cup of tea?", "Still hungry?"
@@ -21,14 +14,12 @@ function starter() {
   document.getElementById("pass").innerHTML = placeholders[Math.floor(Math.random() * (placeholders.length))];
 }
 
-function randFood(){
-  var foods = loadFile("./foods.txt").split("\n");
-  var randFood = foods[Math.floor(Math.random() * (foods.length))];
-  return randFood;
-}
-
 function pass() {
+  var randFood1 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood2 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood3 = foods[Math.floor(Math.random() * (foods.length))];
+  var randFood4 = foods[Math.floor(Math.random() * (foods.length))];
   var seperator = "-";
-  document.getElementById("pass").innerHTML = randFood().concat(seperator.concat(randFood().concat(seperator.concat(randFood().concat(seperator.concat(randFood()))))));
+  document.getElementById("pass").innerHTML = randFood1.concat(seperator.concat(randFood2.concat(seperator.concat(randFood3.concat(seperator.concat(randFood4))))));
   document.getElementById("gen").innerHTML = button[Math.floor(Math.random() * (button.length))];
 }
